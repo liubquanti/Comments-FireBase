0c3a8490a954153ba0841938fdbfb929732c7a35
Oleh Liubchenko <doroleijn@gmail.com>
2023-09-27 20:19:39 +0200

    add

---
+++ script.js
@@ -0,0 +1,41 @@
+// Ініціалізуємо Firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyBC6DE1C4XUXpU0JY0w1SVqweQTGtnFNx0",
+    authDomain: "lq-cm-tst.firebaseapp.com",
+    databaseURL: "https://lq-cm-tst-default-rtdb.firebaseio.com",
+    projectId: "lq-cm-tst",
+    storageBucket: "lq-cm-tst.appspot.com",
+    messagingSenderId: "395966889397",
+    appId: "1:395966889397:web:376f7d4e2faaaf8383edfc"
+};
+firebase.initializeApp(firebaseConfig);
+
+// Посилання на базу даних
+var database = firebase.database().ref("comments");
+
+// Функція для надсилання коментаря
+function submitComment() {
+    var name = document.getElementById("name").value;
+    var comment = document.getElementById("comment").value;
+
+    if (name && comment) {
+        var newCommentRef = database.push();
+        newCommentRef.set({
+            name: name,
+            comment: comment
+        });
+    }
+
+    document.getElementById("name").value = "";
+    document.getElementById("comment").value = "";
+}
+
+// Виведення коментарів
+database.on("child_added", function(snapshot) {
+    var comment = snapshot.val();
+    var commentsDiv = document.getElementById("comments");
+
+    var commentElement = document.createElement("div");
+    commentElement.innerHTML = "<strong>" + comment.name + ":</strong> " + comment.comment;
+    commentsDiv.appendChild(commentElement);
+});
